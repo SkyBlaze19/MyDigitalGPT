58a9d02e2af3e68a8f674456a17c798f10f8d007
Théo Lemaigre <theolemaigrepro@hotmail.com>
2023-06-08 15:02:34 +0200

    Add function mail doublon check test

--- test.php
+++ test.php
@@ -13,6 +13,7 @@
 
     require 'vendor/autoload.php';
 
+
     //phpinfo();
     /*echo date_default_timezone_get();
 
@@ -32,7 +33,7 @@
     $currentDate = date('Y-m-d H:i:s');
     echo $currentDate;
 
-    echo "<br><br>";
+    echo "<hr><br><br>";
     $testF1 = UserModel::username_is_valid('Tlemaigre');
     echo $testF1;
 
@@ -40,3 +41,37 @@
     $testF2 = UserModel::username_is_valid('Tlemaigr');
     if($testF2 == false)
         echo "Cet utilisateur n'existe pas !";
+
+    echo "<hr><br><br>";
+    $testMail1 = check_if_mail_already_used('theolemaigre@hotmail.fr');
+    echo $testMail1;
+
+    echo "<br><br>";
+    $testMail2 = check_if_mail_already_used('a');
+    if($testMail2 == false)
+        echo "Cet email n'est pas utilisé !";
+
+
+        function check_if_mail_already_used($email){
+            $database = Database::getInstance();
+            $conn = $database->getConnection();
+        
+            $query = "SELECT email FROM users WHERE email = :email";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+        
+            $user = $stmt->fetch(PDO::FETCH_ASSOC); // Récupérer les données de l'utilisateur
+        
+            if ($user) {
+                // Un user possède cet email
+                // Faites quelque chose avec les données de l'utilisateur
+                return true;
+                // Faites ce que vous devez faire avec l'utilisateur trouvé
+            } else {
+                // Aucun utilisateur trouvé avec cet email
+                // Faites ce que vous devez faire lorsque l'utilisateur n'est pas trouvé
+                return false;
+            }
+        
+        }
