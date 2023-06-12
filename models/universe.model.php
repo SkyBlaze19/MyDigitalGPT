a0324440810fdafa5350971411d33ef4b306944a
Théo Lemaigre <theolemaigrepro@hotmail.com>
2023-06-12 16:52:32 +0200

    add universe model

---
+++ models/universe.model.php
@@ -0,0 +1,217 @@
+<?php
+
+
+    class UniverseModel {
+
+        private int $id;
+        private string $name;
+        private int $creator_id;
+        private DateTime $created_at; //trasnformer en datetime
+        private DateTime $updated_at;
+
+        public function __construct($id, $name, $creator_id, $created_at, $updated_at)
+        {
+            $this->setId($id);
+            $this->setName($name);
+            $this->setCreatorId($creator_id);
+            $this->setCreatedAt($created_at);
+            $this->setUpdatedAt($updated_at);
+        }
+
+        /* Inutile je pense pour le moment
+        public function toMap(): Array
+        {
+            $monTab = array();
+
+            $monTab['id'] = $this->id;
+            $monTab['name'] = $this->name;
+            $monTab['creator_id'] = $this->creator_id;
+            $monTab['created_at'] = $this->created_at;
+            $monTab['updated_at'] = $this->updated_at;
+
+            // Debug 
+            //print_r($monTab);
+
+            return $monTab;
+            //return array
+        }
+        */
+        
+        
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of name
+         */ 
+        public function getName()
+        {
+                return $this->name;
+        }
+
+        /**
+         * Set the value of name
+         *
+         * @return  self
+         */ 
+        public function setName($name)
+        {
+                $this->name = $name;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of creator_id
+         */ 
+        public function getCreatorId()
+        {
+                return $this->creator_id;
+        }
+
+        /**
+         * Set the value of creator_id
+         *
+         * @return  self
+         */ 
+        public function setCreatorId($creator_id)
+        {
+                $this->creator_id = $creator_id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of created_at
+         */ 
+        public function getCreatedAt()
+        {
+                return $this->created_at;
+        }
+
+        /**
+         * Set the value of created_at
+         *
+         * @return  self
+         */ 
+        public function setCreatedAt($created_at)
+        {
+                $this->created_at = DateTime::createFromFormat('Y-m-d H:i:s', $created_at);
+                return $this;
+        }
+
+        /**
+         * Get the value of updated_at
+         */ 
+        public function getUpdatedAt()
+        {
+                return $this->updated_at;
+        }
+
+        /**
+         * Set the value of updated_at
+         *
+         * @return  self
+         */ 
+        public function setUpdatedAt($updated_at)
+        {
+            $this->updated_at = DateTime::createFromFormat('Y-m-d H:i:s', $updated_at);
+            return $this;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
+        public static function password_is_valid($username, $password){
+            //$conn = connexionBDD();
+            //Debug
+            //echo 'Tu est dans password validate';
+        
+            $database = Database::getInstance();
+            $conn = $database->getConnection();
+        
+            $query = "SELECT username, `password` FROM users WHERE username = :username";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':username', $username);
+        
+            $stmt->execute();
+        
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+            //echo "User password = ".$user['password']." / alors que password = ".$password." !";
+
+            if($user['password'] != $password)
+            {
+                //echo 'Passwd différent !';
+                return false;
+            }
+            else 
+            {
+                //echo 'theoriquement passws ok ';
+                return true;
+            }
+        }
+        
+        public static function username_is_valid($username){
+            //$conn = connexionBDD();
+        
+            $database = Database::getInstance();
+            $conn = $database->getConnection();
+        
+            $query = "SELECT username FROM users WHERE username = :username";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':username', $username);
+        
+            $stmt->execute();
+        
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+            //print_r($user);
+        
+            if(empty($user)) 
+                return false;
+            else 
+                return true;
+        }
+        */
+    }
